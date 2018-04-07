97cf33bf296b46f187eb3d95992047cb2fb430de
Pietro Carnelli <33876294+pc0179@users.noreply.github.com>
2018-04-07 11:53:46 +0100

    Update RomeTaxiGlobalVars.py

--- RomeTaxiGlobalVars.py
+++ RomeTaxiGlobalVars.py
@@ -9,3 +9,7 @@ DatumLong = 12.492373
 #global DatumLat
 DatumLat = 41.890251
 
+#map bounds in paper
+#(41.856, 12.442),(41.928,12.5387)
+#8x8Km grid about rome centre
+#32*32 grid sampling to generate 'grey square heat maps' (250*250m)
