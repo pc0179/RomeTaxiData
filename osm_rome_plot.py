b44eb4380a319b8ef7cf71aeacb2156b276ba015
pc0179 <pc0179@bristol.ac.uk>
2018-04-11 10:21:17 +0100

    towards a pipeline

--- osm_rome_plot.py
+++ osm_rome_plot.py
@@ -8,10 +8,21 @@ fig, ax = plt.subplots(figsize=(10,15))
 m = Basemap(
 	resolution = 'c',
 	projection = 'merc',
-	llcrnrlon=-99.22, llcrnrlat= 19.33, urcrnrlon=-99.12, urcrnrlat=19.45)
+	llcrnrlon=12.442, llcrnrlat= 41.856, urcrnrlon=12.5387, urcrnrlat= 41.928)
+
+#(12.442 41.856, 12.5387 41.928, 12.442 41.928, 12.5387 41.856, 12.442 41.856)
 
 #m.fillcontinents(color='#f0f0f0')
 
+#road_shp_file = '/home/pdawg/RomeOSMdata/extract/extracted_Roman_roads'
+
 m.readshapefile('extracted_Roman_roads', 'roads', drawbounds = True, color='grey')
 
-plt.savefig('rome_road_network.png')
+plt.savefig('/home/pdawg/RomeTaxiData/rome_road_network.png')
+
+
+
+
+#extract relevant trace data
+#extract relevant osm map data
+#plot both..
